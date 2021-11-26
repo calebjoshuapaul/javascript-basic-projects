cc78dc499414c4b2042b17a2ccea0d1c52d15434
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-11-27 00:04:27 +0530

    setup app.js

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -15,6 +15,12 @@ btns.forEach(function (btn) {
       count++;
     } else count = 0;
 
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else value.style.color = "black";
+
     value.textContent = count;
   });
 });
